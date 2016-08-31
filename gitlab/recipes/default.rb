d6e35b4f515cfae4c8a0dc4f87d93dd03a3eef8c
ashok <ashokkumar@freeunixsolutions>
2016-08-31 05:47:52 +0000

    add new repo

---
+++ gitlab/recipes/default.rb
@@ -0,0 +1,5 @@
+git  "/usr/share/nginx/html/recycalize" do
+  repository "http://ashok.kumar:kashnia12345@krepo.kelltontech.net:27001/recycalrepo/recycalize.git"
+  revision "master"
+  action :sync
+end
