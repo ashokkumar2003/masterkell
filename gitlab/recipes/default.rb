807d29b5b153df78ded803d503c0f510dcc28f1f
Ashok <ashok.kumar@kelltontech.com>
2017-02-20 09:06:54 +0000

    updated

--- gitlab/recipes/default.rb
+++ gitlab/recipes/default.rb
@@ -1,9 +1,3 @@
-#git  "/var/www/html/codebase" do
-#  repository "https://github.com/ashokkumar2003/codebase.git"
-#  revision "master"
-#  action :sync
-#end
-
 git "/var/www/html/ashok/temprepo" do
   repository "https://kellgitadmin:KelL23gadm@gitlab.kelltontech.net/temprepo/temprepo.git"
   reference "master"
