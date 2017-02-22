c3a1b93b8d5d32211b1f9b836055a088b1ba5e4c
Ashok <ashok.kumar@kelltontech.com>
2017-02-22 06:30:11 +0000

    upadedfole

--- gitlab/recipes/default.rb
+++ gitlab/recipes/default.rb
@@ -5,8 +5,8 @@
 #end
 
 git "/var/www/html/ashok/temprepo" do
-  repository "https://kellgitadmin:KelL23gadm@gitlab.kelltontech.net/temprepo/temprepo.git"
-  reference "master"
+  repository "https://gitlab.kelltontech.net/temprepo/temprepo.git"
+  reference `master`
   action :sync
 end
 
